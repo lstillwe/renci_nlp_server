41ecee4d5b88f93ee7bb6675141da7bdc624fa7a
Lisa Stillwell <lisa@renci.org>
2016-12-20 15:37:44 -0500

    start shell for starting postgres and nlp app

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 su -l postgres -c '/usr/pgsql-9.4/bin/pg_ctl start -s -l /var/lib/pgsql/9.4/pgsql.log -D /var/lib/pgsql/9.4/data'
 sleep 5
-psql -a -f renci_nlp_server/create_db.sql
-psql -d nlp -a -f renci_nlp_server/setup_db.sql
+su -l postgres -c 'psql -a -f renci_nlp_server/create_db.sql'
+su -l postgres -c 'psql -d nlp -a -f renci_nlp_server/setup_db.sql'
