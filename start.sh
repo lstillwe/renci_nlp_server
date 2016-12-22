0b668bd451c7d630764d46bb2a3e645915468926
Lisa Stillwell <lisa@renci.org>
2016-12-22 13:04:30 -0500

    starting to finalize what start shell will do

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 # get postgres running and load database and tables for nlp server
-su -l postgres -c 'nohup /usr/pgsql-9.4/bin/pg_ctl start -s -l /var/lib/pgsql/9.4/pgsql.log -D /var/lib/pgsql/9.4/data &  >>start.log 2>&1'
+su -l postgres -c 'nohup /usr/pgsql-9.4/bin/pg_ctl start -s -l /var/lib/pgsql/9.4/pgsql.log -D /var/lib/pgsql/9.4/data >nohup.out 2>&1 &'
 sleep 5
 su -l postgres -c 'psql -a -f /renci_nlp_server/create_db.sql >>start.log 2>&1'
 su -l postgres -c 'psql -d nlp -a -f /renci_nlp_server/setup_db.sql >>start.log 2>&1'
